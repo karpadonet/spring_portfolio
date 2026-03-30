12f68294e0642655b88baf2f6e12d4ebf0ce5d95
Alexandra Spivak <shuraspivak@gmail.com>
2026-03-30 13:37:11 +0200

    Finalized Spring Boot structure at root

---
+++ src/main/java/com/alexspivak/my_cv_app/MyCvAppApplication.java
@@ -0,0 +1,13 @@
+package com.alexspivak.my_cv_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexspivak/my_cv_app/MyCvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexspivak.my_cv_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
